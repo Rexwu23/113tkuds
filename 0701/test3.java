397fef565efd0006c648fc435630347396297f02
TKU-FCP-411631137 <mfc930223@gmail.com>
2025-07-01 10:37:39 +0800

    12

---
+++ 0701/test3.java
@@ -0,0 +1,17 @@
+public class test3{
+
+    public static int sum(int[] arr, int n){
+        int total = 0;
+        for (int i = 0; i < n ; i++){
+            total += arr[i];
+        }
+        return total;
+
+    }
+
+    public static void main(String[] args) {
+        int [] data = {10, 20, 30, 40, 50};
+        int result = sum(data, data.length);
+        System.out.println("這列總合為" +result);
+    }
+}
